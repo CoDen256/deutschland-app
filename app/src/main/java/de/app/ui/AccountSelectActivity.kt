6ae7db18902a84bca28d656046d4186a83c74765
Kirill <klarsky.kirill6@gmail.com>
2022-05-03 15:18:07 +0200

    2 acts added blank

---
+++ app/src/main/java/de/app/ui/AccountSelectActivity.kt
@@ -0,0 +1,12 @@
+package de.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import de.app.R
+
+class AccountSelectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_select)
+    }
+}
