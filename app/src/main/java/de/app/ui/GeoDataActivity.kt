9613118c11d0fc0095bff053ed58f79732922f36
Denys Chernyshov <den.blackshov@gmail.com>
2022-05-22 19:26:10 +0200

    Implement MapView

--- app/src/main/java/de/app/ui/GeoDataActivity.kt
+++ app/src/main/java/de/app/ui/GeoDataActivity.kt
@@ -1,58 +1,57 @@
 package de.app.ui
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.StrictMode
 import android.widget.ArrayAdapter
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapView
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import de.app.R
 import de.app.databinding.ActivityGeoDataBinding
 
-class GeoDataActivity : AppCompatActivity(), OnMapReadyCallback {
+class GeoDataActivity : AppCompatActivity() {
 
-    private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityGeoDataBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val mapTilerKey = getMapTilerKey()
+//        validateKey(mapTilerKey)
+        val styleUrl = "https://api.maptiler.com/maps/streets/style.json?key=${mapTilerKey}";
+        // Get the MapBox context
+        Mapbox.getInstance(this, null)
+
+
         binding = ActivityGeoDataBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-
-        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy);
 
         val items = arrayOf("Shutzgebiete", "Umwelt", "Landwirtschaft")
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
         binding.dropdown.setAdapter(adapter)
+
+
+        val mapView: MapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync { map ->
+            // Set the style after mapView was loaded
+            map.setStyle(styleUrl) {
+                map.uiSettings.setAttributionMargins(15, 0, 0, 15)
+                // Set the map view center
+                map.cameraPosition = CameraPosition.Builder()
+                    .target(LatLng(47.127757, 8.579139))
+                    .zoom(10.0)
+                    .build()
+            }
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun getMapTilerKey(): String? {
+        return packageManager.getApplicationInfo(
+            packageName,
+            PackageManager.GET_META_DATA
+        ).metaData.getString("com.maptiler.simplemap.mapTilerKey")
     }
 }
