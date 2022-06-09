013f963a7de0d8729ebddfa3ca093630c3ce6b69
Denys Chernyshov <den.blackshov@gmail.com>
2022-06-09 23:44:12 +0200

    Add dashboard fake info

--- app/src/main/java/de/app/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/de/app/ui/dashboard/DashboardFragment.kt
@@ -7,16 +7,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import de.app.R
+import de.app.core.AccountDataSource
+import de.app.core.SessionManager
 import de.app.databinding.FragmentDashboardBinding
 import de.app.notifications.Notificator
 
 class DashboardFragment : Fragment() {
-
-    private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -24,26 +21,31 @@ class DashboardFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this)[DashboardViewModel::class.java]
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val account = AccountDataSource().getAccounts()[0]
+
+        genFakeNotifications()
+
+
+        binding.welcome.text = getString(R.string.welcome_dashboard,
+            "${account.name} ${account.surname}")
+
+        binding.burgerId.text = getString(R.string.account_id_dashboard, account.accountId)
+        val plz = "06217"
+        val address = "Merseburg"
+        binding.address.text = getString(R.string.address_dashboard, plz, address)
+
+        return root
+    }
+
+    private fun genFakeNotifications() {
         val notificator = Notificator(requireContext())
         notificator.sendEmergencyNotification("There is an emergency in your area")
         notificator.sendLawChangeNotification("There are new laws")
         notificator.sendNewMailNotification("Your document ist abgelaufen")
-
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
