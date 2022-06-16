843f0e35d2a8df568726c198fd98f603ad18d9ff
Denys Chernyshov <den.blackshov@gmail.com>
2022-06-16 23:20:13 +0200

    Finish selecting fragment

--- app/src/main/java/de/app/ui/account/select/SelectAccountFragment.kt
+++ app/src/main/java/de/app/ui/account/select/SelectAccountFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -39,23 +40,34 @@ class SelectAccountFragment : Fragment() {
 
         val adapter = ListViewAdapter(elementBinding, headers) { element, b ->
             b.accountName.text = element.displayName
-            b.root.onClickNavigate(navController, R.id.action_nav_select_to_enter_pin,
+            b.root.onClickNavigate(navController,
+                R.id.action_nav_select_to_enter_pin,
                     "accountId" to element.id
                 )
         }
+        binding.addAccount.onClickNavigate(navController, R.id.action_nav_select_to_register)
 
         binding.accounts.adapter = adapter
         binding.accounts.layoutManager = LinearLayoutManager(context)
 
-        binding.addAccount.onClickNavigate(navController, R.id.action_nav_select_to_register)
-
-        viewLifecycleOwner.lifecycleScope.launch {
-            val accounts = viewModel.getAccounts()
-            headers.addAll(accounts)
-            adapter.notifyItemRangeInserted(0, accounts.size)
-        }
+        initialFetch(headers, adapter)
 
         return binding.root
+    }
+
+    private fun initialFetch(
+        headers: ArrayList<AccountHeader>,
+        adapter: ListViewAdapter<AccountHeader, FragmentLoginSelectAccountItemBinding>
+    ) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            val accounts = viewModel.getAccounts()
+            if (accounts.isEmpty()) {
+                binding.empty.isVisible = true
+            } else {
+                headers.addAll(accounts)
+                adapter.notifyItemRangeInserted(0, accounts.size)
+            }
+        }
     }
 
 }
