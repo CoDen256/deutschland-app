506dcc160f06ad11491c008bd8cfc0005ad041d8
Denys Chernyshov <den.blackshov@gmail.com>
2022-06-19 13:39:58 +0200

    finally works

--- app/src/main/java/de/app/ui/geo/GeoDataMapFragment.kt
+++ app/src/main/java/de/app/ui/geo/GeoDataMapFragment.kt
@@ -1,6 +1,7 @@
 package de.app.ui.geo
 
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,12 +11,19 @@ import androidx.lifecycle.LiveData
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.plugins.annotation.CircleManager
+import com.mapbox.mapboxsdk.plugins.annotation.CircleOptions
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
+import com.mapbox.mapboxsdk.style.sources.GeoJsonOptions
+import com.mapbox.mapboxsdk.utils.ColorUtils
 import dagger.hilt.android.AndroidEntryPoint
 import de.app.R
 import de.app.core.onSuccess
 import de.app.databinding.FragmentGeoDataTabMapBinding
 import de.app.geo.LocationRepository
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class GeoDataMapFragment(private val data: LiveData<MapObjectInfo>) : Fragment() {
@@ -43,20 +51,31 @@ class GeoDataMapFragment(private val data: LiveData<MapObjectInfo>) : Fragment()
             binding.currentObject.text = getString(R.string.selected_object, it.name)
         }
 
-        repo.requestAddress(requireContext()).onSuccess {  result ->
-            result.onSuccess {
-                binding.currentPosition.text = getString(R.string.your_position_is, it.getAddressLine(0))
-            }
-        }
         return binding.root
     }
 
-    private fun setupMap(mapView: MapView, savedInstanceState: Bundle?) {
-        mapView.onCreate(savedInstanceState)
+    private fun setupMap(binding: FragmentGeoDataTabMapBinding, savedInstanceState: Bundle?) {
+        binding.mapView.onCreate(savedInstanceState)
+
+        repo.requestAddress(requireContext()).onSuccess { result ->
+            result.onSuccess {
+                binding.currentPosition.text =
+                    getString(R.string.your_position_is, it.getAddressLine(0))
+
+            }
+        }
+
         mapView.getMapAsync { map ->
             // Set the style after mapView was loaded
             map.setStyle(styleUrl) {
                 map.uiSettings.isAttributionEnabled = false
+                val circleManager = CircleManager(mapView, map, it)
+                val circleOptions = CircleOptions()
+                    .withLatLng(LatLng(51.3563, 11.9917))
+                    .withCircleColor(ColorUtils.colorToRgbaString(Color.RED))
+                    .withCircleRadius(5f)
+                circleManager.create(circleOptions)
+
                 // Set the map view center
                 map.cameraPosition = CameraPosition.Builder()
                     .target(LatLng(51.3563, 11.9917))
