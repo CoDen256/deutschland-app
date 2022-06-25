475e6421e4c2eab8c07affc8d5e9988174bfce92
Denys Chernyshov <den.blackshov@gmail.com>
2022-06-25 21:59:19 +0200

    Fix button position

--- app/src/main/java/de/app/ui/service/SubmittedResultFragment.kt
+++ app/src/main/java/de/app/ui/service/SubmittedResultFragment.kt
@@ -1,11 +1,14 @@
 package de.app.ui.service
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import de.app.databinding.FragmentAdministrativeServiceResultBinding
+
 
 class SubmittedResultFragment:Fragment() {
     override fun onCreateView(
@@ -14,7 +17,10 @@ class SubmittedResultFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentAdministrativeServiceResultBinding.inflate(inflater, container, false)
-
+        val url = "https://coden256.github.io/deutschland-app/"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
         return binding.root
     }
 }
