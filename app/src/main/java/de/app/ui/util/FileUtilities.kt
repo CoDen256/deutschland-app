62d3ed2202c053265bed6ff3a0035a8a73f727c2
Denys Chernyshov <den.blackshov@gmail.com>
2022-06-26 15:41:06 +0200

    finally opens files

--- app/src/main/java/de/app/ui/util/FileUtilities.kt
+++ app/src/main/java/de/app/ui/util/FileUtilities.kt
@@ -7,10 +7,10 @@ import android.content.Intent.ACTION_VIEW
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.StrictMode
-import android.os.StrictMode.VmPolicy
+import android.os.Environment
 import android.provider.OpenableColumns
 import android.webkit.MimeTypeMap
+import androidx.core.content.FileProvider
 import androidx.documentfile.provider.DocumentFile
 import java.io.File
 import java.net.HttpURLConnection
@@ -55,18 +55,19 @@ fun Uri.getFile(contentResolver: ContentResolver): ByteArray? {
 }
 
 fun Context.openFile(uri: Uri, type: String) {
-    val builder = VmPolicy.Builder()
-    StrictMode.setVmPolicy(builder.build())
+    val root = Environment.getExternalStorageDirectory().toString()
+    val dir = File("$root/de-app")
+    if (!dir.exists()){
+        dir.mkdirs()
+    }
+    dir.setWritable(true)
     contentResolver.takePersistableUriPermission(uri,
         Intent.FLAG_GRANT_READ_URI_PERMISSION
             or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
 
     val doc: DocumentFile = DocumentFile.fromSingleUri(this, uri)!!
-    val temp = File.createTempFile(
-        "cw_",
-        "."+MimeTypeMap.getSingleton().getExtensionFromMimeType(doc.type),
-        filesDir
-    )
+
+    val temp = File(dir, "1"+doc.name)
     temp.outputStream().use {
         contentResolver.openInputStream(uri)?.copyTo(it)
     }
@@ -75,7 +76,7 @@ fun Context.openFile(uri: Uri, type: String) {
     intent.action = ACTION_VIEW
     intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    intent.setDataAndType(doc.uri, doc.type)
+    intent.setDataAndType(FileProvider.getUriForFile(this, applicationContext.packageName+".provider",temp ), doc.type)
     intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
     startActivity(Intent.createChooser(intent, "blabla"))
 }
