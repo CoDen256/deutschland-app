b4fb38748206faf2a281aa4d0677ca6d5be121f2
Denys Chernyshov <den.blackshov@gmail.com>
2022-06-28 15:58:38 +0200

    Fix appointment service

--- app/src/main/java/de/app/ui/components/ListFragment.kt
+++ app/src/main/java/de/app/ui/components/ListFragment.kt
@@ -26,9 +26,11 @@ abstract class ListFragment<B: ViewBinding, I: ViewBinding, M> :SimpleFragment<B
         return root
     }
 
+    abstract override fun inflate(inflater: LayoutInflater, container: ViewGroup?): B
     abstract fun inflateChild(inflater: LayoutInflater, container: ViewGroup?): I
     abstract fun setupChild(binding: I, item: M)
     abstract fun loadItems(): List<M>
+    abstract override fun setup()
 
     open fun inflateAndSetupChildren() =
         ListViewAdapter({l, v -> inflateChild(l,v)}, items) { item, binding -> setupChild(binding, item)
