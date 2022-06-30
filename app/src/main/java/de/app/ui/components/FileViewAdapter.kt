07ddf761a7fd448a5b9162d5347d460c0011997e
Denys Chernyshov <den.blackshov@gmail.com>
2022-06-30 15:55:55 +0200

    tryign

--- app/src/main/java/de/app/ui/components/FileViewAdapter.kt
+++ app/src/main/java/de/app/ui/components/FileViewAdapter.kt
@@ -1,6 +1,8 @@
 package de.app.ui.components
 
 import android.app.Activity
+import android.widget.TextView
+import de.app.R
 import de.app.data.model.FileHeader
 import de.app.databinding.CommonFileItemBinding
 import de.app.ui.util.loadFirstPage
@@ -14,13 +16,14 @@ class OpenableFileViewAdapter(
     fileHeaders: List<FileHeader>,
     onLongClickListener: (FileHeader) -> Unit = {},
 ) : FileViewAdapter(fileHeaders, activity, onClickListener = { activity().openFile(it.uri, it.mimeType) },
-    onLongClickListener = onLongClickListener
+    onLongClickListener = {  }
     )
 
 open class FileViewAdapter(
     fileHeaders: List<FileHeader>,
     activity: () -> Activity,
     onLongClickListener: (FileHeader) -> Unit = {},
+    onCreateContextMenuListener: (FileHeader) -> Unit = {},
     onClickListener: (FileHeader) -> Unit,
 ) : ListViewAdapter<FileHeader, CommonFileItemBinding>(
     { inflater, parent -> CommonFileItemBinding.inflate(inflater, parent, false) },
@@ -35,10 +38,16 @@ open class FileViewAdapter(
                 }
             }
         }
-        binding.file.setOnClickListener { onClickListener(file) }
-        binding.file.setOnLongClickListener {
-            onLongClickListener(file)
-            true
+        binding.root.setOnClickListener { onClickListener(file) }
+        binding.root.setOnCreateContextMenuListener { menu, v, menuInfo ->
+            val textView = TextView(activity())
+            menu.add(textView)
+            menu.add(0, 0,0, activity().getString(R.string.file_context_remove))
+            menu.add(0, 1,1, activity().getString(R.string.file_context_download))
+        }
+        binding.root.setOnLongClickListener { binding.file.showContextMenu(0f, 0f) }
+        binding.root.setOnContextClickListener {
+            it.
         }
         binding.fileName.text = cutName(file.name)
     }
