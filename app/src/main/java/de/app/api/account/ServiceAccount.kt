a7b1d82e630c66e4c3e87f48ec08600f34e064b6
Denys Chernyshov <den.blackshov@gmail.com>
2022-07-01 13:57:23 +0200

    add doc

--- app/src/main/java/de/app/api/account/ServiceAccount.kt
+++ app/src/main/java/de/app/api/account/ServiceAccount.kt
@@ -4,21 +4,50 @@ import de.app.data.model.Address
 import java.time.LocalDate
 
 /**
- * Secret Token wird ver
+ * Das [SecretToken] wird erhalten, wenn der Nutzer
+ * sich bei der Servicekonto-API authentifiziert. Dieses Token
+ * wird in den nachfolgenden Abläufen verwendet, um
+ * zusätzliche Informationen für den jeweiligen Nutzer durch
+ * ein [ServiceAccount] abzufragen
  *
- * @property token
- * @constructor Create empty Secret token
+ * @property token das Token
  */
 data class SecretToken(
     val token: String
 )
 
+/**
+ * Repräsentiert ein generisches Servicekonto. Es gibt nur
+ * 2 mögliche Typen des Servicekontos:
+ * 1) Servicekonto für Bürger [CitizenServiceAccount]
+ * 2) Servicekonto für Unternehmen [CompanyServiceAccount]
+ *  Enthält alle notwendige Informationen bezüglich eines Nutzers
+ *  bzw. Unternehmens
+ *
+ * @property accountId ID des Kontos
+ * @property displayName Anzeigename
+ * @property address aktuelle Wohnadresse
+ */
 sealed interface ServiceAccount{
     val accountId: String
     val displayName: String
     val address: Address
 }
 
+/**
+ * [CitizenServiceAccount] repräsentiert ein Servicekonto
+ * für Bürger
+ *
+ * @property accountId Konto ID (wird verwendet, um sich bei Servicekonto-API zu authentfizieren)
+ * @property displayName Anzeigename des Bürgers
+ * @property address Wohnadresse des Bürgers
+ * @property firstName Vorname des Bürgers
+ * @property surname  Nachname
+ * @property salutation Anrede
+ * @property dateOfBirth Geburtsdatum
+ * @property nationality Staatsbürgerschaft
+ * @property placeOfBirth Geburtsort
+ */
 data class CitizenServiceAccount(
     override val accountId: String,
     override val displayName: String,
@@ -31,6 +60,16 @@ data class CitizenServiceAccount(
     val placeOfBirth: Address = address,
     ): ServiceAccount
 
+/**
+ * [CompanyServiceAccount] repräsentiert ein Servicekonto für ein Unternehmen
+ *
+ * @property accountId - Konto ID
+ * @property displayName - Anzeigename
+ * @property address - Addresse des Unternehmens
+ * @property fullName - voller Name des Unternehmens
+ * @property foundedDate - Gründungsdatum
+ * @constructor Create empty Company service account
+ */
 data class CompanyServiceAccount(
     override val accountId: String,
     override val displayName: String,
--- app/src/main/java/de/app/api/account/ServiceAcountRepository.kt
+++ app/src/main/java/de/app/api/account/ServiceAcountRepository.kt
@@ -1,5 +1,6 @@
 package de.app.api.account
 
+
 interface CitizenServiceAccountRepository {
     fun getCitizenAccountSecretToken(accountId: String): Result<SecretToken>
     fun getCitizenAccount(secretToken: SecretToken): Result<CitizenServiceAccount>
