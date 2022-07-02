93d949bf09a4ae0ddf511bc31c2a20d03b4a1fc9
Denys Chernyshov <den.blackshov@gmail.com>
2022-07-02 22:22:43 +0200

    trying dashboard

--- deutschland-app/src/main/java/de/app/ui/dashboard/DashboardFragment.kt
+++ deutschland-app/src/main/java/de/app/ui/dashboard/DashboardFragment.kt
@@ -22,7 +22,6 @@ class DashboardFragment : AccountAwareFragment<FragmentDashboardBinding>() {
 
     override fun setup() {
         fillHeader(account)
-        fillDashboardInfo()
     }
 
     private fun fillHeader(account: ServiceAccount) {
@@ -41,54 +40,5 @@ class DashboardFragment : AccountAwareFragment<FragmentDashboardBinding>() {
                 }
             )
         }
-    }
-
-    private fun fillDashboardInfo() {
-        inflateSection(
-            binding.appointments, "Applications", listOf(
-                "19.10.2000" to "You have an appointment at doctor",
-                "20.09.2010" to "Appointment to pick up the document",
-                "19.05.2022" to "Appointment to visit residence",
-                "19.05.2022" to "Appointment to visit residence"
-            )
-        )
-        inflateSection(
-            binding.applications, "Applications", listOf(
-                "Application #1" to "Done",
-                "Application #2" to "Pending",
-                "Application #3" to "Sent",
-                "Application #4" to "Pending"
-            )
-        )
-        inflateSection(
-            binding.emergencies, "Emergencies", listOf(
-                "Emergency #1" to "There is an emergency in your are",
-                "Emergency #2" to "Flooding and strong winds in Merseburg",
-                "Emergency #3" to "Hot weather in Merseburg",
-                "Emergency #4" to "Too much snow"
-            )
-        )
-    }
-
-    private fun inflateSection(
-        binding: FragmentDashboardSectionBinding,
-        sectionTitle: String,
-        cards: List<Pair<String, String>>
-    ) {
-
-        binding.sectionTitle.text = sectionTitle
-        inflateCard(binding.first, cards[0])
-        inflateCard(binding.second, cards[1])
-        inflateCard(binding.third, cards[2])
-        inflateCard(binding.fourth, cards[3])
-    }
-
-    private fun inflateCard(
-        item: FragmentDashboardAppointmentItemBinding,
-        itemData: Pair<String, String>
-    ) {
-        val (caption, body) = itemData
-        item.body.text = body
-        item.caption.text = caption
     }
 }
