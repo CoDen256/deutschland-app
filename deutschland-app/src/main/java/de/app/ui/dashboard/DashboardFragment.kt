a737e0bee345ba465b95ae747b29e2c6d81fb37f
Denys Chernyshov <den.blackshov@gmail.com>
2022-07-02 22:35:52 +0200

    polish dashboard

--- deutschland-app/src/main/java/de/app/ui/dashboard/DashboardFragment.kt
+++ deutschland-app/src/main/java/de/app/ui/dashboard/DashboardFragment.kt
@@ -1,19 +1,28 @@
 package de.app.ui.dashboard
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
 import dagger.hilt.android.AndroidEntryPoint
 import de.app.R
 import de.app.api.account.ServiceAccount
 import de.app.api.account.CitizenServiceAccount
 import de.app.api.account.CompanyServiceAccount
+import de.app.core.SessionManager
+import de.app.data.model.UserHeader
 import de.app.databinding.FragmentDashboardAppointmentItemBinding
 import de.app.databinding.FragmentDashboardBinding
 import de.app.databinding.FragmentDashboardSectionBinding
+import de.app.databinding.FragmentUserSelectItemBinding
 import de.app.ui.components.AccountAwareFragment
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class DashboardFragment : AccountAwareFragment<FragmentDashboardBinding>() {
+
+    @Inject
+    lateinit var sessionManager: SessionManager
 
     override fun inflate(
         inflater: LayoutInflater,
@@ -37,6 +46,16 @@ class DashboardFragment : AccountAwareFragment<FragmentDashboardBinding>() {
                     "${account.salutation} ${account.firstName} ${account.surname}"
                 is CompanyServiceAccount -> account.fullName
             }
+            sessionManager.currentUser?.let {
+                renderIcon(it)
+            }
         }
+    }
+
+    private fun renderIcon(item: UserHeader) {
+        GlideToVectorYou.init()
+            .with(requireContext())
+            .setPlaceHolder(R.drawable.img, R.drawable.img)
+            .load(Uri.parse("https://avatars.dicebear.com/api/jdenticon/${item.userId}.svg"), binding.icon)
     }
 }
